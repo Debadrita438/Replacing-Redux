8ffdf732eb682b9892d92a3ebff9a40b76065b55
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-27 17:13:15 +0530

    Initial Project

---
+++ src/store/reducers/productSlice.js
@@ -0,0 +1,52 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    products: [
+        {
+            id: 'p1',
+            title: 'Red Scarf',
+            description: 'A pretty red scarf.',
+            isFavorite: false
+        },
+        {
+            id: 'p2',
+            title: 'Blue T-Shirt',
+            description: 'A pretty blue t-shirt.',
+            isFavorite: false
+        },
+        {
+            id: 'p3',
+            title: 'Green Trousers',
+            description: 'A pair of lightly green trousers.',
+            isFavorite: false
+        },
+        {
+            id: 'p4',
+            title: 'Orange Hat',
+            description: 'Street style! An orange hat.',
+            isFavorite: false
+        }
+    ]
+};
+
+const productSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+        toggleFav: (state, action) => {
+            const prodIndex = state.products.findIndex((p) => p.id === action.payload);
+            console.log(prodIndex);
+            const newFavStatus = !state.products[prodIndex].isFavorite;
+            const updatedProducts = [ ...state.products ];
+            updatedProducts[prodIndex] = {
+                ...state.products[prodIndex],
+                isFavorite: newFavStatus
+            };
+            state.products = updatedProducts;
+        }
+    }
+});
+
+export const productActions = productSlice.actions;
+
+export default productSlice.reducer;
